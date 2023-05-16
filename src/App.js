70abaae740b754fed5ea28fccd1ef7c01d0b06ca
Shinajr <lasisioluwashina@gmail.com>
2023-05-16 14:14:20 +0100

    react project 1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Hello, I am Shina from Nigeria</h1>
+      <p>It is a honour for me to be here</p>
+    </div>
+  );
+}
+
+export default App;
